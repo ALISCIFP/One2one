e0383ba019bbba61a9c67872a67106121690c3eb
Zack Shen <szm0219@gmail.com>
2019-01-28 20:19:29 -0600

    no recgan

--- train_cycle/horse2zebra.sh
+++ train_cycle/horse2zebra.sh
@@ -1,10 +1,10 @@
 set -ex
 python train.py \
 --dataroot ./datasets/horse2zebra \
---name horse2zebra_cyclegan_self \
+--name horse2zebra_cyclegan_self_noRecGan \
 --model cycle_gan \
 --pool_size 50 \
 --no_dropout \
 --display_port 8098 \
---gpu_ids 1
+--gpu_ids 0
 
