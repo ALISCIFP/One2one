ae7fb0a550a381f8dc1ced11266cf51afca97108
Zack <szm0219@gmail.com>
2019-01-28 20:49:21 -0600

    two new

--- train_cycle/horse2zebra_unet256.sh
+++ train_cycle/horse2zebra_unet256.sh
@@ -1,10 +1,11 @@
 set -ex
 python train.py \
 --dataroot ./datasets/horse2zebra \
---name horse2zebra_cyclegan_self_noRecGan \
+--name horse2zebra_cyclegan_self_noRecGan_unet256 \
 --model cycle_gan \
 --pool_size 50 \
 --no_dropout \
---display_port 8099 \
---gpu_ids 0
+--display_port 8100 \
+--gpu_ids 1 \
+--netG unet_256
 
