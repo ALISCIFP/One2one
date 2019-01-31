0534c1458f843990a006c5e6c370f871a32db3a5
Zack Shen <szm0219@gmail.com>
2019-01-31 01:12:45 -0600

    city

--- train_cyclegan/cityscapes.sh
+++ train_cyclegan/cityscapes.sh
@@ -1,7 +1,7 @@
 set -ex
 python train.py \
 --dataroot ./datasets/cityscapes \
---name cityscapes_cyclegan \
+--name cityscapes_cyclegan_Self_cycle_2Dis_noRec \
 --model cycle_gan \
 --pool_size 50 \
 --no_dropout \
