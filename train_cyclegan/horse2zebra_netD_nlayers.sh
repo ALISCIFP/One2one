012d9da97eb37cf63854c5e92d81594c6b776e63
Zack <szm0219@gmail.com>
2019-02-01 23:43:58 -0600

    gan286

--- train_cyclegan/horse2zebra_netD_nlayers.sh
+++ train_cyclegan/horse2zebra_netD_nlayers.sh
@@ -1,11 +1,11 @@
 set -ex
 python train.py \
 --dataroot ./datasets/horse2zebra \
---name horse2zebra_cyclegan_selfcycle_2Dis_286Gan \
+--name horse2zebra_cyclegan_selfcycle_2Dis_noRecGan_286Gan \
 --model cycle_gan \
 --pool_size 50 \
 --no_dropout \
---display_port 8098 \
+--display_port 8096 \
 --gpu_ids 1 \
 --netD n_layers \
 --n_layers_D 5
