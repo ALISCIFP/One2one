8c9e1a829d6c0a1ebbd288f44289b6af43ac756e
Zack <szm0219@gmail.com>
2019-02-05 17:44:54 -0600

    test

---
+++ test_cyclegan/horse2zebra.sh
@@ -0,0 +1,9 @@
+set -ex
+python train.py \
+--dataroot ./datasets/horse2zebra \
+--name horse2zebra_cyclegan_selfcycle_2Dis_noRecGan \
+--model cycle_gan \
+--pool_size 50 \
+--no_dropout \
+--display_port 8100 \
+--gpu_ids 1
