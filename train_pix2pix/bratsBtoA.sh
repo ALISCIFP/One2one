bd797b9fe5ebb5d207a9820673cb532a06d047e4
Zack <szm0219@gmail.com>
2019-02-26 21:40:29 -0600

    brats

---
+++ train_pix2pix/bratsBtoA.sh
@@ -0,0 +1,15 @@
+set -ex
+python train.py \
+--dataroot ./datasets/BratsT1T2 \
+--name BratsT1T2_pix2pix_BtoA \
+--model pix2pix \
+--netG unet_256 \
+--direction BtoA \
+--lambda_L1 100 \
+--dataset_mode aligned \
+--norm batch \
+--pool_size 0 \
+--display_id 0 \
+--gpu_ids 0 \
+--input_nc 1 \
+--output_nc 1
