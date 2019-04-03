99ea5a29bf9d63a3869113a687f3fec0d7f0ce39
Zack Shen <szm0219@gmail.com>
2019-04-03 16:55:06 -0500

    seploss

--- train_cyclegan/horse2zebra_netD_nlayers.sh
+++ train_cyclegan/horse2zebra_netD_nlayers.sh
@@ -1,12 +1,13 @@
 set -ex
 python train.py \
 --dataroot ./datasets/horse2zebra \
---name horse2zebra_cyclegan_selfcycle_2Dis_noRecGan_286Gan_unet256 \
+--name horse2zebra_cyclegan_selfcycle_2Dis_noRecGan_SepLoss_286D \
 --model cycle_gan \
 --pool_size 50 \
 --no_dropout \
---display_port 8096 \
---gpu_ids 1 \
+--display_port 8097 \
+--gpu_ids 0 \
 --netD n_layers \
 --n_layers_D 5 \
---netG unet_256
+--lambda_identity 0.1 
+
