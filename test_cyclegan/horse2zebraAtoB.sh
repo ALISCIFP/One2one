be016e8d8438240b3fa22754c414e25e9a265723
Zack Shen <szm0219@gmail.com>
2019-04-05 00:33:53 -0500

    add pretrain

--- test_cyclegan/horse2zebraAtoB.sh
+++ test_cyclegan/horse2zebraAtoB.sh
@@ -1,11 +1,10 @@
 set -ex
 python test.py \
 --dataroot ./datasets/horse2zebra \
---name horse2zebra_cyclegan \
+--name horse2zebra_pretrained \
 --model cycle_gan \
 --no_dropout \
 --gpu_ids 0 \
 --phase test \
 --direction AtoB \
---epoch 200 \
 --num_test 120
--- test_cyclegan/horse2zebraBtoA.sh
+++ test_cyclegan/horse2zebraBtoA.sh
@@ -1,11 +1,10 @@
 set -ex
 python test.py \
 --dataroot ./datasets/horse2zebra \
---name horse2zebra_cyclegan \
+--name zebra2horse_pretrained \
 --model cycle_gan \
 --no_dropout \
 --gpu_ids 0 \
 --phase test \
-# --direction BtoA \
---epoch 200 \
+--direction BtoA \
 --num_test 140
