146dee0cfe10792c135c33815602fd848bbf0b7d
Zack <szm0219@gmail.com>
2019-04-06 03:08:27 -0500

    up

--- train_cyclegan/cityscapes.sh
+++ train_cyclegan/cityscapes.sh
@@ -1,8 +1,9 @@
 set -ex
 python train.py \
---dataroot ./datasets/cityscapes_patial \
---name cityscapes_patial_cyclegan \
+--dataroot ./datasets/cityscapes \
+--name cityscapes_cyclegan_Self_cycle_2Dis_noRec_SepLoss \
 --model cycle_gan \
 --pool_size 50 \
 --no_dropout \
---display_port 8097
+--display_port 8098 \
+--gpu_ids 1 
--- train_cyclegan/horse2zebra.sh
+++ train_cyclegan/horse2zebra.sh
@@ -1,8 +1,12 @@
 set -ex
 python train.py \
 --dataroot ./datasets/horse2zebra \
---name horse2zebra_cyclegan \
+--name horse2zebra_cyclegan_selfcycle_2Dis_noRecGan_SepLoss \
 --model cycle_gan \
 --pool_size 50 \
 --no_dropout \
---display_id 0
+--display_port 8099 \
+--gpu_ids 0 \
+--continue_train \
+--lambda_identity 0.1 \
+--epoch_count 100
