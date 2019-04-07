6ac3214c9b386b3987f7e7693d713d3b06431c5f
Zack Shen <szm0219@gmail.com>
2019-04-07 17:36:19 -0500

    upo

--- test_cyclegan/bratsA2B.sh
+++ test_cyclegan/bratsA2B.sh
@@ -2,7 +2,7 @@ set -ex
 python test.py \
 --dataroot ./datasets/BratsT1T2 \
 --dataset_mode aligned \
---name BratsT1T2_cyclegan_Self_cycle_2Dis_noRec_SepLoss \
+--name BratsT1T2_cyclegan \
 --model cycle_gan \
 --direction AtoB \
 --dataset_mode aligned \
@@ -10,5 +10,5 @@ python test.py \
 --epoch 4 \
 --input_nc 1 \
 --output_nc 1 \
---score_dir ./results2/bratsT1T2_cycle_sepLoss4.csv \
+--score_dir ./results2/bratsT1T2_cycle4.csv \
 --results_dir ./results2/
