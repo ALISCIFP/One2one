d80b3ba2074daf924e2389f98153e7ea567ec5bb
Zack <szm0219@gmail.com>
2019-04-08 08:10:39 -0500

    up

--- test_cyclegan/bratsA2B.sh
+++ test_cyclegan/bratsA2B.sh
@@ -4,11 +4,12 @@ python test.py \
 --dataset_mode aligned \
 --name BratsT1T2_cyclegan_Self_cycle_2Dis_noRec_SepLoss \
 --model cycle_gan \
+--gpu_ids 1 \
 --direction AtoB \
 --dataset_mode aligned \
 --num_test 10230 \
---epoch 5 \
+--epoch 6 \
 --input_nc 1 \
 --output_nc 1 \
---score_dir ./results2/bratsT1T2_cycle_sepLoss5.csv \
---results_dir ./results2/
+--score_dir ./results/bratsT1T2_cycle_sepLoss6.csv \
+--results_dir ./results/
