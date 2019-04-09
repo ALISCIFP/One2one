d3dea21f57f07a75d00cd40e2546d4ccd00d7f84
Zack Shen <szm0219@gmail.com>
2019-04-08 20:20:43 -0500

    as

--- test_cyclegan/horse2zebra.sh
+++ test_cyclegan/horse2zebra.sh
@@ -1,11 +1,10 @@
 set -ex
 python test.py \
 --dataroot ./datasets/horse2zebra \
---name /home/zack/GitHub/One2one/checkpoints/horse2zebra_cyclegan_selfcycle_2Dis_noRecGan_SepLoss_286D \
+--name horse2zebra_cyclegan_selfcycle_2Dis_noRecGan_SepLoss_Resume200Idt_lambdaB1 \
 --model cycle_gan \
 --no_dropout \
 --gpu_ids 0 \
 --phase test \
 --direction AtoB \
---epoch 200 \
 --num_test 120
